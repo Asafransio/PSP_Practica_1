90935cab1d50c781dd0ba2a8f2be37386b163edc
Asafransio <fran.kisko98@gmail.com>
2020-12-01 13:55:21 +0100

    entrega 2 psp

--- PSP/src/practica2/Principal.java
+++ PSP/src/practica2/Principal.java
@@ -1,14 +1,87 @@
 package practica2;
 
+
+
+import java.util.Random;
 import java.util.Scanner;
 
 
 
-public class Principal extends Thread{
 
+
+public class Principal extends Thread{
+	
 	public static int cantidadJinetes;
 	public static int distanciaMeta;
-	public Jinete camello;
+	static boolean finCarrera = false;
+	
+	
+	public static class Camello implements Runnable{
+		
+		String name;
+		int recorrido = 0;
+		int recorre;
+		
+		Random random = new Random();
+		int[] falta = new int[cantidadJinetes];
+		
+		
+		public Camello(String string) {
+			name = string;
+		}
+			
+	
+		public void run() {
+			
+			while(finCarrera==false) {
+				
+				try {
+					Thread.sleep(new Random().nextInt(2000));
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				recorre = anda(recorrido, random.nextInt(100));
+				
+				muestra(recorre);
+				
+			}
+			
+		}
+
+		public synchronized void muestra(int recorre) {
+			int restante;
+			
+			if (recorrido<distanciaMeta) {
+				recorrido += recorre;
+				restante = distanciaMeta - recorrido;
+				restante = fijarCero(restante);
+				
+				String nombre = currentThread().getName();
+				int numero = Integer.parseInt(nombre.substring(7));
+				
+				falta[numero-1]=restante;
+				
+				System.out.println(currentThread().getName() + " ha avanzado " + recorre + " metros, está a " + falta[numero-1] + " metros de la meta.");
+				
+			}
+			else if (recorrido>=distanciaMeta){
+				finCarrera=true;
+				System.out.println(currentThread().getName() + " ha avanzado " + recorre + " metros, está a " + falta[Integer.parseInt(currentThread().getName().substring(7))-1] + " metros de la meta.");
+				System.out.println(currentThread().getName() + "ha ganado la carrera.");
+				for (int i = 1; i <= cantidadJinetes; i++) {
+					if (i!=Integer.parseInt(currentThread().getName().substring(7))) {
+						System.out.println("Camello"+i+" ha quedado a "+ falta[i-1] + " metros de la meta.");
+					}
+				}
+				System.exit(-1);
+			}
+			
+		}
+		
+	}
+
+	
+	
 	
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
@@ -22,21 +95,64 @@ public class Principal extends Thread{
 		try {
 			for (int i=1; i<=cantidadJinetes;i++) {
 				
-				Jinete j = new Jinete("Camello" + (i));
-				Thread t = new Thread(j);
+				Camello c = new Camello("Camello" + i);
+				Thread t = new Thread(c);
+				t.setName("Camello" + i);
 				t.start();
 			}
+			
+			
 		}
 		catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
-		
-		
+			
+	
 		
 		teclado.close();
 	}
 	
 	
+	
+	public static int anda(int recorrido, int avanza) {
+		
+		int corre;
+		
+		
+		
+			if (avanza < 30) {
+			
+				corre = 0;
+				
+			}
+		
+			if ((avanza >= 30)&&(avanza < 70)) {
+			
+				corre = 1;					
+				
+			}
+		
+			if ((avanza >=70)&&(avanza < 90)) {
+			
+				corre = 2;
+			
+			}
+		
+			else {
+			
+				corre = 3;				
+				
+			}
+			return corre;	
+	}
+
+	public static int fijarCero (int numero) {
+		if (numero<0) {
+			return 0;
+		}
+		else {
+			return numero;
+		}
+	}
 	
 }
